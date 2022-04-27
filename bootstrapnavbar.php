4961073f10607d1b1fc7bb6355f2efaa13347196
tesla10001 <saumyadubeyec1058@gmail.com>
2022-04-27 05:51:05 +0000

    testing

---
+++ bootstrapnavbar.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Nav menu</title>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	 <link rel="stylesheet" href=
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	
+	<script src=
+"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+	</script>
+	
+	<script src=
+"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+	</script>
+	
+	<script src=
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+	</script> 
+</head>
+
+<body>
+	
+	<div class="container">
+		
+		
+		
+		<h2 style="text-align:center;">
+			Justified Tabs/pills Nav Menu
+		</h2>
+		
+		
+		
+		<ul class="nav nav-pills nav-justified">
+			<li class="nav-item">
+				<a class="nav-link active" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">About Us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Pricing</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Contact Us</a>
+			</li>
+		</ul>
+	</div>
+</body>
+
+</html>				
+
